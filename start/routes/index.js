724ed33a719bc94be60cd739ca8ffdbb30cdd547
Justin Seawell <jpseawell@gmail.com>
2020-11-08 16:38:22 -0600

    Move artist routes to their own file

--- start/routes/index.js
+++ start/routes/index.js
@@ -13,39 +13,10 @@
 |
 */
 
-/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use("Route");
 
 require("./website");
 require("./password");
 require("./api-v1");
 require("./admin");
-
-// ======================================
-// ARTIST PORTAL
-// ======================================
-Route.group(() => {
-  Route.get("/apply", "HomeController.home"); // Redirect old apply page
-
-  Route.get("/", "HomeController.home").as("artist.home");
-  Route.post("users", "UserController.store").as("artist.user.store");
-  Route.get("login", "UserController.login").as("artist.user.login");
-  Route.post("login", "UserController.authenticate").as("artist.user.auth");
-})
-  .namespace("Artist")
-  .prefix("artists");
-
-Route.group(() => {
-  Route.get("songs/create", "SongController.create").as("artist.songs.create");
-  Route.post("songs/store", "SongController.store").as("artist.songs.store");
-  Route.get("songs", "SongController.index").as("artist.songs.index");
-
-  Route.get("profile", "ArtistController.show").as("artist.show");
-  Route.get("profile/edit", "ArtistController.edit").as("artist.edit");
-  Route.post("profile/:id", "ArtistController.update").as("artist.update");
-
-  Route.get("logout", "UserController.logout").as("artist.user.logout");
-})
-  .namespace("Artist")
-  .prefix("artists")
-  .middleware("artist");
+require("./artist");
