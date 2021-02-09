2aded4547956e93807fd270faaa0061662a358e1
Justin Seawell <jpseawell@gmail.com>
2021-02-09 15:07:38 -0600

    Pull password controller from v1 namespace

--- start/routes/password.js
+++ start/routes/password.js
@@ -1,9 +1,11 @@
 const Route = use("Route");
 
 Route.get("auth/reset/:resetToken", "PasswordController.reset")
-.middleware("guest")
-.as("password.edit");
+  .middleware("guest")
+  .namespace("Api/v1")
+  .as("password.edit");
 
 Route.post("auth/reset/:resetToken", "PasswordController.resetPassword")
-.middleware("guest")
-.as("password.update");
+  .middleware("guest")
+  .namespace("Api/v1")
+  .as("password.update");
